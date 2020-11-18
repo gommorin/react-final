00c5c56fd6d1e34bdaacf40086c6aa6a2d327513
Axel de la Torre <axeldelat@gmail.com>
2020-11-17 22:52:53 -0500

    newcommit from dhome

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Componente Home desde development</h1>
+      <h1>Componente Home desde f/home</h1>
     </div>
   );
 }
