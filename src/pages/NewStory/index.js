579b4c7d8fede87329769918b85c5250a4761f6c
David Moranchel <david.arcshtag@gmail.com>
2020-11-17 22:19:38 -0600

    reactstrap

--- src/pages/NewStory/index.js
+++ src/pages/NewStory/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 // Reactstrap
-import { Button } from "reactstrap";
+import { Button, Alert } from "reactstrap";
 
 class NewStory extends Component {
   constructor(props) {
@@ -14,6 +14,7 @@ class NewStory extends Component {
       <div>
         <h1>Componente New Story desde feature/NewStory</h1>
         <Button color="danger">Danger!</Button>
+        <Alert color="primary">This is a primary alert — check it out!</Alert>
       </div>
     );
   }
