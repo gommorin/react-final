38449d3a2812c0873f260033c66feb9b0c0aef05
luisbastida <luisbastida86@gmail.com>
2020-11-18 17:52:06 -0600

    New Story form creation

--- src/pages/NewStory/index.js
+++ src/pages/NewStory/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 // Reactstrap
-import { Button, Alert } from "reactstrap";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 class NewStory extends Component {
   constructor(props) {
@@ -12,9 +12,29 @@ class NewStory extends Component {
   render() {
     return (
       <div>
-        <h1>Componente New Story desde feature/NewStory</h1>
-        <Button color="danger">Danger!</Button>
-        <Alert color="primary">This is a primary alert — check it out!</Alert>
+        <h2 className= "text-muted">Draft</h2>
+        <Form>
+        <FormGroup>
+          <Input type="text" name="Title" id="postTitle" placeholder="Title" className= "text-muted"/>
+        </FormGroup>
+        <FormGroup> 
+          <Input type="text" name="User" id="postUser" placeholder="What's your name?" className= "text-muted"/>  
+        </FormGroup>
+        <FormGroup> 
+          <Input type="text" name="Story" id="postStory" placeholder="Tell your story..." className= "text-muted"/>  
+        </FormGroup>
+        <FormGroup> 
+        <Input type="date" name="Date" id="postDate"  className= "text-muted"/> 
+        </FormGroup>
+        <FormGroup> 
+        <Input type="number" name="minutes" id="postMinutes" placeholder="How long does your post take to be read? (minutes)" className= "text-muted"/> 
+        </FormGroup>
+        <FormGroup> 
+          <Label for="Image" className= "text-muted">Select an image</Label>
+        <Input type="file" name="Image" id="postimage"  className= "text-muted"/> 
+        </FormGroup>
+        <Button>Publish</Button>
+        </Form>
       </div>
     );
   }
