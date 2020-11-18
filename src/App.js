85daf18941209acfabee6df3e2fbf0ae35f097d3
David Moranchel <david.arcshtag@gmail.com>
2020-11-17 21:25:47 -0600

    remane NewStory page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
+import "./App.css";
 
 // Páginas
-import Home from './pages/Home'
-import Story from './pages/Story'
-import NewStory from './pages/New-Story'
+import Home from "./pages/Home";
+import Story from "./pages/Story";
+import NewStory from "./pages/NewStory";
 
 // React Router
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-  <Router>
-    <div>
+    <Router>
+      <div>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path='/new-story' component={NewStory} />
-          <Route path='/story' component={Story} />
+          <Route path="/new-story" component={NewStory} />
+          <Route path="/story" component={Story} />
         </Switch>
-    </div>
-  </Router>
+      </div>
+    </Router>
   );
 }
 
