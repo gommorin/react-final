670102d1b6bfc4c655a97d29057e7b8a6fc1bdba
David Moranchel <david.arcshtag@gmail.com>
2020-11-17 21:32:04 -0600

    development initial commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-  <div>
-    <h1>Componente Home</h1>
-  </div>
-  )
+    <div>
+      <h1>Componente Home desde development</h1>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
