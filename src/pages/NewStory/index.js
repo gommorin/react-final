829728fd6745a30f3e80b46c23b2ab3d1227c851
DevHaro <harojose2019@gmail.com>
2020-11-19 15:24:03 -0600

    File index.js changed, using fetch to add new entry (POST)

--- src/pages/NewStory/index.js
+++ src/pages/NewStory/index.js
@@ -9,8 +9,8 @@ class NewStory extends Component {
     super(props);
     this.state = {
       title: "",
-      user: "",
-      story: "",
+      author: "",
+      content: "",
       date: "",
       minutes: "",
       image: ""
@@ -28,12 +28,29 @@ class NewStory extends Component {
   }
 
   handleSubmit(event) {
-    event.preventDefault();
-    console.log("Le toca a Haro")
+    event.preventDefault();    
+    const { title, author, content, date, time, image } = this.state;
+    const newEntry = {
+      title: title,
+      author: author,
+      content: content,
+      date: date,
+      timeToRead: time,
+      imageUrl: image
+    }
+    console.log(newEntry)
+
+    fetch("http://localhost:8080/entries/",{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newEntry)
+    });
   }
 
   render() {
-    const { title, user, story, date, minutes, image } = this.state;
+    const { title, author, content, date, time, image } = this.state;
     return (
       <div>
         <h2 className= "text-muted">Draft</h2>
@@ -42,16 +59,16 @@ class NewStory extends Component {
           <Input type="text" value={title} name="title" onChange={this.handleNewPostChange} placeholder="Title" className= "text-muted"/>
         </FormGroup>
         <FormGroup>
-          <Input type="text" value={user} name="user" onChange={this.handleNewPostChange} placeholder="What's your name?" className= "text-muted"/>  
+          <Input type="text" value={author} name="author" onChange={this.handleNewPostChange} placeholder="What's your name?" className= "text-muted"/>  
         </FormGroup>
         <FormGroup> 
-          <Input type="text" value={story} name="story" onChange={this.handleNewPostChange} placeholder="Tell your story..." className= "text-muted"/>  
+          <Input type="text" value={content} name="content" onChange={this.handleNewPostChange} placeholder="Tell your story..." className= "text-muted"/>  
         </FormGroup>
         <FormGroup> 
         <Input type="date" value={date} name="date" onChange={this.handleNewPostChange}  className= "text-muted"/> 
         </FormGroup>
         <FormGroup> 
-        <Input type="number" value={minutes} name="minutes" onChange={this.handleNewPostChange} placeholder="How long does your post take to be read? (minutes)" className= "text-muted"/> 
+        <Input type="number" value={time} name="time" onChange={this.handleNewPostChange} placeholder="How long does your post take to be read? (minutes)" className= "text-muted"/> 
         </FormGroup>
         <FormGroup> 
         <Input type="text" value={image} name="image" onChange={this.handleNewPostChange} placeholder="Write the image URL" className= "text-muted"/> 
