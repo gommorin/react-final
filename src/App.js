9e5c7bdbe42f06e3601706a2bbf7170c30126aac
luisbastida <luisbastida86@gmail.com>
2020-11-19 21:50:45 -0600

    Fix App.js for NavBar and Rename components directory

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Story from "./pages/Story";
 import NewStory from "./pages/NewStory";
-import NavBar from "./Components/NavBar"
+import NavBar from "./components/NavBar"
 
 // React Router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -13,11 +13,11 @@ function App() {
   return (
     <Router>
       <div>
+        <NavBar/>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/new-story" component={NewStory} />
           <Route path="/story" component={Story} />
-          <Route path="/navbar" component={NavBar} />
         </Switch>
       </div>
     </Router>
