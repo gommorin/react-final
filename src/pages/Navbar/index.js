c56381bed6894e98f0dd315aa1643e56e2395f9f
luisbastida <luisbastida86@gmail.com>
2020-11-19 20:32:46 -0600

    Nav bar creation

---
+++ src/pages/Navbar/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+// Reactstrap
+import { Button, Form, FormGroup, Input, Container, Row, Col } from "reactstrap";
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+}
