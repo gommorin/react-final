0d1ea45612cb4d3d519b213b5aaf75d4b1de1716
luisbastida <luisbastida86@gmail.com>
2020-11-19 20:09:21 -0600

    Styles finished

--- src/pages/NewStory/index.js
+++ src/pages/NewStory/index.js
@@ -12,7 +12,7 @@ class NewStory extends Component {
       author: "",
       content: "",
       date: "",
-      minutes: "",
+      time: "",
       image: ""
     };
 
@@ -60,22 +60,36 @@ class NewStory extends Component {
               <Form onSubmit={this.handleSubmit}>
                 <Col xs="12" md={{ size: 8, offset: 2 }} className= "p-0">
                   <FormGroup>
-                    <Input type="text" value={title} name="title" onChange={this.handleNewPostChange} placeholder="Title" className= "text-muted"/>
+                    <Input type="text" value={title} name="title" onChange={this.handleNewPostChange} placeholder="Title" className= "text-muted border border-right-0 border-top-0 border-bottom-0 border-secondary rounded-0 "/>
                   </FormGroup>
                 </Col>
-              <FormGroup>
-                <Input type="text" value={author} name="user" onChange={this.handleNewPostChange} placeholder="What's your name?" className= "text-muted"/>  
-              </FormGroup>
-              <FormGroup> 
-                <Input type="text" value={content} name="story" onChange={this.handleNewPostChange} placeholder="Tell your story..." className= "text-muted"/>  
-              </FormGroup>
-              <FormGroup> 
-              <Input type="date" value={date} name="date" onChange={this.handleNewPostChange}  className= "text-muted"/> 
-              </FormGroup>
-              <FormGroup> 
-              <Input type="text" value={image} name="image" onChange={this.handleNewPostChange} placeholder="Write the image URL" className= "text-muted"/> 
-              </FormGroup>
-              <Button type="submit">Publish</Button>
+                <Col xs="12" md={{ size: 8, offset: 2 }} className= "p-0">
+                  <FormGroup>
+                    <Input type="text" value={author} name="author" onChange={this.handleNewPostChange} placeholder="What's your name?" className= "text-muted border border-right-0 border-top-0 border-bottom-0 border-secondary rounded-0 "/>  
+                  </FormGroup>
+                </Col>
+                <Col xs="12" md={{ size: 8, offset: 2 }} className= "p-0">
+                  <FormGroup> 
+                    <Input type="text" value={content} name="content" onChange={this.handleNewPostChange} placeholder="Tell your story..." className= "text-muted border border-right-0 border-top-0 border-bottom-0 border-secondary rounded-0"/>  
+                  </FormGroup>
+                <Col xs="12" md={{ size: 3}} className= "p-0">
+                  <FormGroup> 
+                    <Input type="date" value={date} name="date" onChange={this.handleNewPostChange}  className= "text-muted border border-right-0 border-top-0 border-bottom-0 border-secondary rounded-0"/> 
+                  </FormGroup>
+                </Col>
+                <Col xs="12" md={{ size: 8, offset: 2 }} className= "p-0"></Col>
+                  <FormGroup> 
+                    <Input type="number" value={time} name="time" onChange={this.handleNewPostChange} placeholder="How long does your story take to be read? " className= "text-muted border border-right-0 border-top-0 border-bottom-0 border-secondary rounded-0"/> 
+                  </FormGroup>
+                </Col>
+                <Col xs="12" md={{ size: 8, offset: 2 }} className= "p-0">
+                  <FormGroup> 
+                    <Input type="text" value={image} name="image" onChange={this.handleNewPostChange} placeholder="Write the image URL" className= "text-muted border border-right-0 border-top-0 border-bottom-0 border-secondary rounded-0"/> 
+                  </FormGroup>
+                </Col>
+              <Col xs="12" md={{ size: 8, offset: 2 }} className= "p-0">
+                <Button type="submit" className= "w-100 bg-light text-secondary border-light ">Publish</Button>
+              </Col>
               </Form>
             </Col>
           </Row>
