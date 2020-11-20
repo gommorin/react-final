e7751c6ca76933cc4532910656b08d926b9eead0
luisbastida <luisbastida86@gmail.com>
2020-11-19 21:39:24 -0600

    Navbar elements and style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Story from "./pages/Story";
 import NewStory from "./pages/NewStory";
+import NavBar from "./Components/NavBar"
 
 // React Router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/new-story" component={NewStory} />
           <Route path="/story" component={Story} />
+          <Route path="/navbar" component={NavBar} />
         </Switch>
       </div>
     </Router>
