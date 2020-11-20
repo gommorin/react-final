9064b802c4003d98b836cad3781145e5e561858e
luisbastida <luisbastida86@gmail.com>
2020-11-19 19:13:13 -0600

    Changes in stye

--- src/pages/NewStory/index.js
+++ src/pages/NewStory/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 // Reactstrap
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Button, Form, FormGroup, Input, Container, Row, Col } from "reactstrap";
 
 
 class NewStory extends Component {
@@ -36,28 +36,36 @@ class NewStory extends Component {
     const { title, user, story, date, minutes, image } = this.state;
     return (
       <div>
-        <h2 className= "text-muted">Draft</h2>
-        <Form onSubmit={this.handleSubmit}>
-        <FormGroup>
-          <Input type="text" value={title} name="title" onChange={this.handleNewPostChange} placeholder="Title" className= "text-muted"/>
-        </FormGroup>
-        <FormGroup>
-          <Input type="text" value={user} name="user" onChange={this.handleNewPostChange} placeholder="What's your name?" className= "text-muted"/>  
-        </FormGroup>
-        <FormGroup> 
-          <Input type="text" value={story} name="story" onChange={this.handleNewPostChange} placeholder="Tell your story..." className= "text-muted"/>  
-        </FormGroup>
-        <FormGroup> 
-        <Input type="date" value={date} name="date" onChange={this.handleNewPostChange}  className= "text-muted"/> 
-        </FormGroup>
-        <FormGroup> 
-        <Input type="number" value={minutes} name="minutes" onChange={this.handleNewPostChange} placeholder="How long does your post take to be read? (minutes)" className= "text-muted"/> 
-        </FormGroup>
-        <FormGroup> 
-        <Input type="text" value={image} name="image" onChange={this.handleNewPostChange} placeholder="Write the image URL" className= "text-muted"/> 
-        </FormGroup>
-        <Button type="submit">Publish</Button>
-        </Form>
+        <Container>
+          <h2 className= "text-muted">Draft</h2>
+          <Row>
+            <Col>
+              <Form onSubmit={this.handleSubmit}>
+                <Col xs="12" md={{ size: 8, offset: 2 }} className= "p-0">
+                  <FormGroup>
+                    <Input type="text" value={title} name="title" onChange={this.handleNewPostChange} placeholder="Title" className= "text-muted"/>
+                  </FormGroup>
+                </Col>
+              <FormGroup>
+                <Input type="text" value={user} name="user" onChange={this.handleNewPostChange} placeholder="What's your name?" className= "text-muted"/>  
+              </FormGroup>
+              <FormGroup> 
+                <Input type="text" value={story} name="story" onChange={this.handleNewPostChange} placeholder="Tell your story..." className= "text-muted"/>  
+              </FormGroup>
+              <FormGroup> 
+              <Input type="date" value={date} name="date" onChange={this.handleNewPostChange}  className= "text-muted"/> 
+              </FormGroup>
+              <FormGroup> 
+              <Input type="number" value={minutes} name="minutes" onChange={this.handleNewPostChange} placeholder="How long does your post take to be read? (minutes)" className= "text-muted"/> 
+              </FormGroup>
+              <FormGroup> 
+              <Input type="text" value={image} name="image" onChange={this.handleNewPostChange} placeholder="Write the image URL" className= "text-muted"/> 
+              </FormGroup>
+              <Button type="submit">Publish</Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>  
       </div>
     );
   }
